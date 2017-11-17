49e4e6e6d397244237f6f2ddbcdaef67154e28fe
Dalgona <dalgona@hontou.moe>
2017-11-17 13:31:12 +0900

    Wrap all HTTP GET requests. Needs code simplification

--- public/assets/js/planner-client.js
+++ public/assets/js/planner-client.js
@@ -1,12 +1,12 @@
 (function (win) {
   /**
-   * Constructor function of PlannerClient objects.
+   * Constructor function of AjaxWrapper objects.
+   *
+   * AjaxWrapper wraps all required AJAX requests into methods.
    * @param {string} apiRoot The root URL of all API resources
    */
-  function PlannerClient(apiRoot) {
+  function AjaxWrapper(apiRoot) {
     const API_ROOT = apiRoot;
-
-    this.currentUser = null;
 
     /**
      * Send an AJAX request to the API server.
@@ -53,7 +53,7 @@
     };
   }
 
-  PlannerClient.prototype = {
+  AjaxWrapper.prototype = {
     // POST /user
 
     // GET /user
@@ -63,10 +63,8 @@
      * @param {(reason: object) => void} onError
      */
     getUserInfo: function (onComplete, onError) {
-      var that = this;
       this._ajax('get', '/user', null,
         function (status, response) {
-          that.currentUser = response;
           onComplete(response);
         }, function (status, reason) {
           onError(reason);
@@ -118,36 +116,124 @@
     },
 
     // GET /planner
+    /**
+     * Retrieve all planners owned by the current user.
+     * @param {(planners: object[]) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getAllPlanners: function (onComplete, onError) {
+      this._ajax('get', '/planner', null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // POST /planner
 
     // GET /planner/:id
+    /**
+     * Retrieve information of selected planner.
+     * @param {number} plannerId
+     * @param {(planner: object) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getPlanner: function (plannerId, onComplete, onError) {
+      this._ajax('get', '/planner/' + plannerId, null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // PUT /planner/:id
 
     // DELETE /planner/:id
 
     // GET /planner/:id/schedule
-
     // GET /planner/:id/schedule/:year
-
     // GET /planner/:id/schedule/:year/:month
-
     // GET /planner/:id/schedule/:year/:month/:day
+    /**
+     * Retrieve schedules associated with the selected planner.
+     * @param {number} plannerId
+     * @param {number[]} range Filter the returned list by starting date.
+     * @param {(schedules: object[]) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getSchedules: function (plannerId, range, onComplete, onError) {
+      var path = range.join('/');
+      this._ajax('get', '/planner/' + plannerId + '/schedule/' + path, null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // POST /planner/:id/schedule
 
     // GET /schedule/:id
+    /**
+     * Retrieve information of the selected schedule.
+     * @param {number} scheduleId
+     * @param {(schedule: object) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getSchedule: function (scheduleId, onComplete, onError) {
+      this._ajax('get', '/schedule/' + scheduleId, null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // PUT /schedule/:id
 
     // DELETE /schedule/:id
 
     // GET /planner/:id/todo-list
+    /**
+     * Retrieve a list of all to-do lists associated with the selected planner.
+     * @param {number} plannerId
+     * @param {(todoLists: object[]) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getAllTodoLists: function (plannerId, onComplete, onError) {
+      this._ajax('get', '/planner/' + plannerId + '/todo-list', null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // POST /planner/:id/todo-list
 
     // GET /todo-list/:id
+    /**
+     * Retrieve information of the selected to-do list.
+     * @param {number} listId
+     * @param {(list: object) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getTodoList: function (listId, onComplete, onError) {
+      this._ajax('get', '/todo-list/' + listId, null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // PUT /todo-list/:id
 
@@ -156,16 +242,60 @@
     // POST /todo-list/:id/item
 
     // GET /todo-item/:id
+    /**
+     * Retrieve information of the selected to-do list item.
+     * @param {number} itemId
+     * @param {(item: object) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getTodoListItem: function (itemId, onComplete, onError) {
+      this._ajax('get', '/todo-item/' + itemId, null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // PUT /todo-item/:id
 
     // DELETE /todo-item/:id
 
     // GET /label
+    /**
+     * Retrieve a list of all labels owned by the currente user.
+     * @param {(labels: object) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getAllLabels: function (onComplete, onError) {
+      this._ajax('get', '/label', null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // POST /label
 
     // GET /label/:id
+    /**
+     * Retrieve information of the selected label.
+     * @param {number} labelId
+     * @param {(label: object) => void} onComplete
+     * @param {(reason: object) => void} onError
+     */
+    getLabel: function (labelId, onComplete, onError) {
+      this._ajax('get', '/label/' + labelId, null,
+        function (status, response) {
+          onComplete(response);
+        }, function (status, reason) {
+          onError(reason);
+        }
+      );
+    },
 
     // PUT /label/:id
 
@@ -173,6 +303,6 @@
   };
 
   win.plannerClientLib = {
-    PlannerClient: PlannerClient,
+    AjaxWrapper: AjaxWrapper, // TODO: hide when completed
   };
 })(window);

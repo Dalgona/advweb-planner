13de6ce334170afe774e17dd065bf2841099d613
Dalgona <dalgona@hontou.moe>
2017-11-23 09:31:24 +0900

    Construct sign-in form UI

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@
   function SignInForm(baseElement) {
     this.element = baseElement;
     this.mode = 0; // 0: sign-in, 1: create account
+    this.submitClicked = null;
 
     var that = this;
     var tabs =
@@ -16,6 +17,7 @@
         }, false);
       })(i);
     }
+
     var heights = [];
     var formItems = this.element.getElementsByClassName('form-item');
     for (var i = 0; i < formItems.length; i++) {
@@ -25,11 +27,96 @@
       formItems[i].setAttribute('data-index', i);
     }
 
+    var inputFields = [
+      document.getElementById('signin-email'),
+      document.getElementById('signin-fullname'),
+      document.getElementById('signin-password'),
+      document.getElementById('signin-confirm')
+    ];
+    var signInButton = document.getElementById('signin-button');
+    for (var i = 0; i < inputFields.length; i++) {
+      inputFields[i].addEventListener('keyup', (function (e) {
+        this.validate();
+      }).bind(this), false);
+    }
+    signInButton.addEventListener('click', (function (e) {
+      e.preventDefault();
+      if (!this.submitClicked) {
+        console.warn('[SignInForm] Please assign appropriate event handler to `submitClicked`.');
+      } else {
+        var args = [];
+        for (var i = 0; i < 4; i++) {
+          args.push(inputFields[i].value);
+        }
+        this.submitClicked.apply(this, args);
+      }
+    }).bind(this), false);
+
+    var errorMsg = this.element.getElementsByClassName('error-message')[0];
+    hide(errorMsg);
+
     /**
      * Reset the state of the sign-in form.
      */
     this.reset = function () {
       this.mode = 0;
+      this.setError(null);
+      for (var i = 0; i < 4; i++) {
+        formItems[i].getElementsByTagName('input')[0].value = '';
+      }
+    };
+
+    /**
+     * Checks if all input fields are filled by the user.
+     */
+    this.allFilled = function () {
+      var acc = true;
+      acc = acc && !!inputFields[0].value;
+      acc = acc && !!inputFields[2].value;
+      if (this.mode == 1) {
+        acc = acc && !!inputFields[1].value;
+        acc = acc && !!inputFields[3].value;
+      }
+      return acc;
+    }
+
+    /**
+     * Checks if the values of two password fields match.
+     */
+    this.passwordMatches = function () {
+      if (this.mode == 0) {
+        return true;
+      }
+      return inputFields[2].value === inputFields[3].value;
+    }
+
+    this.validate = function (setTo) {
+      if (setTo !== undefined) {
+        signInButton.disabled = !setTo;
+        return setTo;
+      }
+      if (!this.allFilled()) {
+        signInButton.disabled = true;
+        return true;
+      }
+      if (!this.passwordMatches()) {
+        signInButton.disabled = true;
+        inputFields[3].classList.add('error');
+        return false;
+      }
+      signInButton.disabled = false;
+      inputFields[3].classList.remove('error');
+      return true;
+    };
+
+    this.setError = function (message) {
+      if (!message) {
+        errorMsg.textContent = '';
+        errorMsg.style.display = 'none';
+      } else {
+        errorMsg.textContent = message;
+        errorMsg.style.display = 'block';
+      }
     }
 
     this.updateUI = function () {
@@ -42,31 +129,44 @@
       switch (this.mode) {
         case 0:
           for (var i = 0; i < elemForSignIn.length; i++) {
-            elemForSignIn[i].style.height = heights[elemForSignIn[i].getAttribute('data-index')];
-            elemForSignIn[i].style.opacity = '1';
-            if (i == elemForSignIn.length - 1) { break; }
-            elemForSignIn[i].style.marginBottom = '0.25rem';
+            show(elemForSignIn[i]);
           }
           for (var i = 0; i < elemForSignUp.length; i++) {
-            elemForSignUp[i].style.height = '0';
-            elemForSignUp[i].style.opacity = '0';
-            elemForSignUp[i].style.marginBottom = '0';
+            hide(elemForSignUp[i]);
           }
+          signInButton.value = 'Sign in';
           break;
         case 1:
           for (var i = 0; i < elemForSignIn.length; i++) {
-            elemForSignIn[i].style.height = '0';
-            elemForSignIn[i].style.opacity = '0';
-            elemForSignIn[i].style.marginBottom = '0';
+            hide(elemForSignIn[i]);
           }
           for (var i = 0; i < elemForSignUp.length; i++) {
-            elemForSignUp[i].style.height = heights[elemForSignUp[i].getAttribute('data-index')];
-            elemForSignUp[i].style.opacity = '1';
-            if (i == elemForSignUp.length - 1) { break; }
-            elemForSignUp[i].style.marginBottom = '0.25rem';
+            show(elemForSignUp[i]);
           }
+          signInButton.value = 'Create Account';
           break;
       }
+      this.validate();
+    };
+
+    function show(element) {
+      element.style.display = '';
+      element.classList.add('anim');
+      setTimeout(function () {
+        element.classList.remove('hidden');
+      }, 5);
+      setTimeout(function () {
+        element.classList.remove('anim');
+      }, 350);
+    }
+
+    function hide(element) {
+      element.classList.add('anim');
+      element.classList.add('hidden');
+      setTimeout(function () {
+        element.classList.remove('anim');
+        element.style.display = 'none';
+      }, 350);
     }
     
     this.reset();
@@ -76,6 +176,7 @@
   win.onload = function (e) {
     win.app = {};
     var signInForm = new SignInForm(document.getElementById('signin-form'));
+
     win.app.signInForm = signInForm;
   }
 })(window);

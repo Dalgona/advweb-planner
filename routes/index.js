82d5577f82d8e79a52b40e6d13e135112f32a4f8
Dalgona <dalgona@hontou.moe>
2017-12-08 04:26:36 +0900

    Server: Load app title from the config file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var commonConfig = require('../config/config.json')['common'];
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: commonConfig.appName });
 });
 
 module.exports = router;
